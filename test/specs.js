69ea840bff74a15afced8a32a438323c21a2dd6a
Mushex Antaranian <mushex@antaranian.com>
2014-07-05 21:01:37 +0400

    print character coordinates

--- test/specs.js
+++ test/specs.js
@@ -5,5 +5,9 @@ var report = require('..');
 describe('reporter', function(){
     it('should log', function(){
         assert.equal(report('Something.'), void 0);
+        assert.equal(report('Something.', {
+            source : 'Hey\n Something',
+            index  : 8
+        }), void 0);
     });
 });
